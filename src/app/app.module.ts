2a10cd183266cee8b8f4c2d26f401918c310749b
Kerry Wang <xuanw@vmware.com>
2019-10-23 14:46:32 +0800

    add antd framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { StoreModule } from '@ngrx/store';
@@ -23,6 +24,7 @@ import { UserComponent } from './users/user/user.component';
   declarations: [AppComponent, UsersComponent, UserComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     SharedModule,
 
     StoreModule.forRoot(appReducers),
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,9 +7,45 @@ import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+import {
+  NzToolTipModule,
+  NzSelectModule,
+  NzSliderModule,
+  NzNotificationModule,
+  NzPopoverModule,
+} from 'ng-zorro-antd';
+
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule, RouterModule],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule, RouterModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+
+    NzToolTipModule,
+    NzSelectModule,
+    NzSliderModule,
+    NzNotificationModule,
+    NzPopoverModule,
+    NzDropDownModule,
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+
+    NzToolTipModule,
+    NzSelectModule,
+    NzSliderModule,
+    NzNotificationModule,
+    NzPopoverModule,
+    NzDropDownModule,
+  ],
   providers: [],
   declarations: [],
 })
